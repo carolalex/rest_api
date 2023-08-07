caf783187d553fbd73e2aff9c898843777be508f
carolalex <carol16121991@gmail.com>
2023-08-07 14:58:30 +0530

    first commit

---
+++ lib/dashboard.dart
@@ -0,0 +1,61 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class ApiIntegration extends StatefulWidget {
+  const ApiIntegration({super.key});
+
+  @override
+  State<ApiIntegration> createState() => _ApiIntegrationState();
+}
+
+class _ApiIntegrationState extends State<ApiIntegration> {
+  List<dynamic> users = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("REST API Integration"),
+      ),
+      body: ListView.builder(
+        itemCount: users.length,
+        itemBuilder: (BuildContext context, int index) {
+          final user = users[index];
+          final name = user['name']['first'];
+          final email = user['email'];
+          final imageUrl = user['picture']['thumbnail'];
+          return ListTile(
+            leading: ClipRRect(
+              borderRadius: BorderRadius.circular(100),
+              child: Image.network(imageUrl),
+            ),
+            title: Text(name),
+            subtitle: Text(email),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: fetchUsers,
+      ),
+    );
+  }
+
+  void fetchUsers() async {
+    try {
+      print("Fetchuser called");
+      const url = 'https://randomuser.me/api/?results=100';
+      final uri = Uri.parse(url);
+      final response = await http.get(uri);
+      final body = response.body;
+      final json = jsonDecode(body);
+      setState(() {
+        users = json['results'];
+      });
+      print("Fetchusers completed");
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
+}
